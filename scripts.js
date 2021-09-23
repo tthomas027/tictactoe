2732e05e52659c8b904e63f75d27c5dca3c518dd
Travis Thomas <tthomas027@gmail.com>
2021-09-22 19:11:36 -0700

    display board on page with temp data

--- scripts.js
+++ scripts.js
@@ -2,11 +2,23 @@ const createPlayer = (name, piece) => {
   return {name, piece};
 }
 
+const playerX = createPlayer('Player X', 'X');
+const playerO = createPlayer('Player O', 'O');
+
 const gameBoard = (() => {
   let board = [];
   for (let i = 0; i < 9; i++) {
-    board.push('');
+    board.push(i); // fill board with temp data
   }
+
+  const boardDisplay = document.querySelector('#boardDisplay');
+
+  board.forEach(element => {
+    const square = document.createElement('div');
+    square.classList.add('square');
+    square.textContent = element; // display temp data
+    boardDisplay.appendChild(square);
+  })
   
   return {
     board
